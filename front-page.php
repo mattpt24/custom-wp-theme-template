e2c3139d5a5865a8f37c2e0f4c6649eebf2e5556
Matt Persell-Thompson <66426542+mattpt24@users.noreply.github.com>
2025-03-07 17:02:35 +0000

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -4,7 +4,12 @@ get_header();
 
 
 
-<!-- CONTENT FOR HOME PAGE GOES HERE -->
+<div class="greeting">
+    <img class="greeting__logo"src="<?php echo get_theme_file_uri('assets/images/wordpress-logo-light.png')?>" alt="White WordPress Logo">
+    <h1 class="greeting__title">Custom WordPress Theme</h1>
+    <h2 class="greeting__subtitle">Delete this in front-page.php and get started!</h2>
+</div>
+
 
 
 <?php
